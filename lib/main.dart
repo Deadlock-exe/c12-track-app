d75fc7d2a911a4e3b2e21f291fc8f024efb32f26
Aditya <itsadityagupta0909@gmail.com>
2024-01-19 19:34:21 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:c12_track/main_ui/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
